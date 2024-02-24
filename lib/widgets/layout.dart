01e2732b69b62928c94eaa085595fbefba2a6504
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-02-24 14:05:41 +0100

    Removed unused imports

--- lib/widgets/layout.dart
+++ lib/widgets/layout.dart
@@ -1,4 +1,3 @@
-import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
